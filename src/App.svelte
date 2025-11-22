0ba58aa5275bbb341119b13e058fe1b37439d8d7
Jed Fonner <gitlab@fonner.dev>
2025-11-22 17:11:37 -0500

    Exposes package.json version on app page

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-  import type { RoadmapItem } from "./types";
+  import type { RoadmapItem } from './types';
   import Roadmap from './lib/Roadmap.svelte';
-  import { onMount } from "svelte";
+  import { onMount } from 'svelte';
+
+  // @ts-ignore
+  const version = __APP_VERSION__;
 
   let status = $state('loading');
   let items: RoadmapItem[] = $state([]);
@@ -17,35 +20,38 @@
       try {
         import('./roadmap-data.json').then((module) => {
           items = module.default as RoadmapItem[];
-          status = "loaded"
+          status = 'loaded';
         });
       } catch (jsonError) {
         console.error('Error loading local JSON data:', jsonError);
-        status = "error"
+        status = 'error';
       }
       return;
     }
     // @ts-ignore
-    await google.script.run.withSuccessHandler((response) => {
-      console.log('Loaded data from Spreadsheet', response);
-      status = "loaded"
-      items = structuredClone(response) as RoadmapItem[];
-    }).withFailureHandler((error:any) => {
-      console.error('Error invoking server function:', error);
-      alert('Failed to invoke server function.');
-    }).getRoadmapData();
-  }
+    await google.script.run
+      .withSuccessHandler((response: any) => {
+        console.log('Loaded data from Spreadsheet', response);
+        status = 'loaded';
+        items = structuredClone(response) as RoadmapItem[];
+      })
+      .withFailureHandler((error: any) => {
+        console.error('Error invoking server function:', error);
+        alert('Failed to invoke server function.');
+      })
+      .getRoadmapData();
+  };
 </script>
 
 <main>
-  <h1>Roadmap v1.8.0</h1>
+  <h1>Roadmap v{version}</h1>
   {#if status === 'loading'}
     <p>Loading...</p>
   {:else if status === 'error'}
     <p>Error loading data.</p>
   {:else}
     <div class="roadmap-container">
-      <Roadmap bind:items={items} />
+      <Roadmap bind:items />
     </div>
   {/if}
 </main>
