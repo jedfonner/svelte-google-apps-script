e152219b90805bd72687547cd2483464eef193ee
Jed Fonner <gitlab@fonner.dev>
2026-01-25 20:57:21 -0500

    Improves favicon and logging

--- gas/Code.js
+++ gas/Code.js
@@ -2,7 +2,8 @@ function doGet(e) {
   return HtmlService.createTemplateFromFile('Index')
     .evaluate()
     .setTitle('Svelte Google Apps Script')
-    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
+    .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL)
+    .setFaviconUrl('https://developers.google.com/static/site-assets/developers_64dp.png')
 }
 
 function include(filename) {
@@ -11,5 +12,10 @@ function include(filename) {
 
 function testInvokationFromClient(arg1, arg2) {
   Logger.log('testInvokationFromClient called with args: ' + arg1 + ', ' + arg2);
-  return 'Success from server - received: ' + arg1 + ', ' + arg2;
+  return 'Success from testInvokationFromClient on server - received: ' + arg1 + ', ' + arg2;
+}
+
+function testInvokationFromClient2(arg1, arg2) {
+  Logger.log('testInvokationFromClient2 called with args: ' + arg1 + ', ' + arg2);
+  return 'Success from testInvokationFromClient2 on server - received: ' + arg1 + ', ' + arg2;
 }
--- src/lib/mocks.ts
+++ src/lib/mocks.ts
@@ -2,11 +2,11 @@
 export const mocks: ServerFunctions = {
   testInvokationFromClient: (arg1, arg2) => {
     console.log('[MOCK] Server function testInvokationFromClient executed');
-    return "Success from mock server - received: " + arg1 + " " + arg2;
+    return "Success from mocked testInvokationFromClient - received: " + arg1 + " " + arg2;
   },
   testInvokationFromClient2: (arg1, arg2) => {
     console.log('[MOCK] Server function testInvokationFromClient2 executed');
-    return "Success from mock server 2 - received: " + arg1 + " " + arg2;
+    return "Success from mocked testInvokationFromClient2 - received: " + arg1 + " " + arg2;
   },
   // You can add more mock server functions here as needed
 }
